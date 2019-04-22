4d1e9468277350d38ce476261540e751e1e80ef3
Sam Selfridge <Sam.selfridge@gmail.com>
2019-04-21 17:52:25 -0700

    updated popup.js visuals

--- popup.js
+++ popup.js
@@ -1,14 +1,14 @@
 'use strict';
-let on_off_button = document.getElementById('on_off_button');
-let pause_button = document.getElementById('pause_button');
+const on_off_button = document.getElementById('on_off_button');
+const pause_button = document.getElementById('pause_button');
 
 //globals for popup.js
 let currentTab;
 let currentTabActive;
 let paused;
 
-const pausedHtml = 'Resume <span id="pauseSubText">(Alt + s)</span>' 
-const resumeHtml = 'Pause <span id="pauseSubText">(Alt + s)</span>' 
+const pausedHtml = 'Resume <span id="pauseSubText">(Alt + s)</span>'
+const resumeHtml = 'Pause <span id="pauseSubText">(Alt + s)</span>'
 
 // update currentTab for popup - not expected to change during its lifecycle
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -16,7 +16,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 });
 
 
-//get status when first opening popup turns on
+//get button status when opening popup
 chrome.storage.sync.get(['activeTabs', 'paused'], function (result) {
 
   const activeTabs = result['activeTabs'];
@@ -26,18 +26,19 @@ chrome.storage.sync.get(['activeTabs', 'paused'], function (result) {
 
   if (currentTabActive) {
     on_off_button.innerHTML = 'Stop'
+    on_off_button.className = 'red'
+
   } else {
     on_off_button.innerHTML = 'Start '
+    on_off_button.className = 'green'
   }
 
   if (paused) {
-    pause_button.innerHTML = 'Resume <span id="pauseSubText">(Alt + s)</span>' 
+    pause_button.innerHTML = pausedHtml;
   } else {
-    pause_button.innerHTML = 'Pause <span id="pauseSubText">(Alt + s)</span>'
+    pause_button.innerHTML = resumeHtml;
   }
-
 });
-
 
 // turn refreshing on or off for the current tab
 on_off_button.onclick = function (element) {
@@ -61,7 +62,9 @@ on_off_button.onclick = function (element) {
 
       chrome.storage.sync.set(newObj, function () {
         on_off_button.innerHTML = 'Start'
+        on_off_button.className = 'green'
       });
+
     } else { //turn extention on for current tab
       activeTabs.push(currentTab);
       let newObj = {
@@ -70,25 +73,25 @@ on_off_button.onclick = function (element) {
 
       chrome.storage.sync.set(newObj, function () {
         on_off_button.innerHTML = 'Stop'
+        on_off_button.className = 'red'
       });
     } //else
   }) //get.sync.storage.chrome
 } //onclick
 
-
 pause_button.onclick = function () {
   chrome.storage.sync.get(['paused'], function (result) { //fetch active tabs from storage
     paused = !result['paused'];
 
-    let newState = {
-      paused: paused,
-    }
-
     let newText;
     if (paused) {
-      newText = 'Resume'
+      newText = pausedHtml;
     } else {
-      newText = 'Pause'
+      newText = resumeHtml;
+    }
+
+    let newState = {
+      paused: paused,
     }
 
     chrome.storage.sync.set(newState, function () {
@@ -97,5 +100,3 @@ pause_button.onclick = function () {
     return;
   });
 }
-
-
