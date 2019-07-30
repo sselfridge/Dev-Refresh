b19d1d516d9407e83843fd496d26b90140ff0bc6
Sam Selfridge <Sam.selfridge@gmail.com>
2019-07-29 19:17:14 -0700

    Adding Logging since it things don't seem to work from the store

--- content.js
+++ content.js
@@ -1,13 +1,13 @@
 
 window.onfocus = function () {
     chrome.storage.sync.set({ userInactive: false }, function () { });
-    // console.log(`User Active`);
+    console.log(`Dev Refresh: User Active`);
 };
 
 window.onblur = function () {
     // //TODO - track edge that user leaves the screen, if right, don't restart refreshing
     chrome.storage.sync.set({ userInactive: true }, function () { });
-    // console.log(`User Inactive`);
+    console.log(`Dev Refresh: User Inactive`);
 };
 
 
