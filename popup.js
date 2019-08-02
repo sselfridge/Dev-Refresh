8afac3e232814a568eed3197dcf4fe3e07563d1e
Sam Selfridge <Sam.selfridge@gmail.com>
2019-08-02 16:53:20 -0700

    Add local links link
    Prettier formatting

--- popup.js
+++ popup.js
@@ -1,36 +1,33 @@
-'use strict';
-const on_off_button = document.getElementById('on_off_button');
-const pause_button = document.getElementById('pause_button');
+"use strict";
+const on_off_button = document.getElementById("on_off_button");
+const pause_button = document.getElementById("pause_button");
 
 //globals for popup.js
 let currentTab;
 let currentTabActive;
 let paused;
 
-const pausedHtml = 'Resume <span id="pauseSubText">(Alt + s)</span>'
-const resumeHtml = 'Pause <span id="pauseSubText">(Alt + s)</span>'
+const pausedHtml = 'Resume <span id="pauseSubText">(Alt + s)</span>';
+const resumeHtml = 'Pause <span id="pauseSubText">(Alt + s)</span>';
 
 // update currentTab for popup - not expected to change during its lifecycle
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
   currentTab = tabs[0].id;
 });
 
-
 //get button status when opening popup
-chrome.storage.sync.get(['activeTabs', 'paused'], function (result) {
-
-  const activeTabs = result['activeTabs'];
-  paused = result['paused'];
+chrome.storage.sync.get(["activeTabs", "paused"], function(result) {
+  const activeTabs = result["activeTabs"];
+  paused = result["paused"];
 
   currentTabActive = activeTabs.includes(currentTab);
 
   if (currentTabActive) {
-    on_off_button.innerHTML = 'Stop'
-    on_off_button.className = 'red'
-
+    on_off_button.innerHTML = "Stop";
+    on_off_button.className = "red";
   } else {
-    on_off_button.innerHTML = 'Start '
-    on_off_button.className = 'green'
+    on_off_button.innerHTML = "Start ";
+    on_off_button.className = "green";
   }
 
   if (paused) {
@@ -41,7 +38,7 @@ chrome.storage.sync.get(['activeTabs', 'paused'], function (result) {
 });
 
 //watch paused and update resume/pause button as needed
-chrome.storage.onChanged.addListener(function (obj) {
+chrome.storage.onChanged.addListener(function(obj) {
   if (obj.paused) {
     console.log(`Paused Change in POPUP`);
     console.debug(`Paused updated: ${obj.paused.newValue}`);
@@ -52,52 +49,53 @@ chrome.storage.onChanged.addListener(function (obj) {
   } else {
     pause_button.innerHTML = resumeHtml;
   }
-
-})
-
+});
 
 // turn refreshing on or off for the current tab
-on_off_button.onclick = function (element) {
-  chrome.storage.sync.get(['activeTabs'], function (result) { //fetch active tabs from storage
-    let activeTabs = result['activeTabs'];
+on_off_button.onclick = function(element) {
+  chrome.storage.sync.get(["activeTabs"], function(result) {
+    //fetch active tabs from storage
+    let activeTabs = result["activeTabs"];
 
     currentTabActive = activeTabs.includes(currentTab);
 
-    console.debug(`Popup.js----Active Tabs ${activeTabs} CurrentTabActive?:${currentTabActive}`);
+    console.debug(
+      `Popup.js----Active Tabs ${activeTabs} CurrentTabActive?:${currentTabActive}`
+    );
 
     if (currentTabActive) {
-
       //Remove current tab from activetabs array
-      let indexOf = activeTabs.indexOf(currentTab)
+      let indexOf = activeTabs.indexOf(currentTab);
       activeTabs.splice(indexOf, 1);
 
       console.debug(`Active Tabs After: ${activeTabs}`);
       let newObj = {
-        activeTabs: activeTabs,
-      }
+        activeTabs: activeTabs
+      };
 
-      chrome.storage.sync.set(newObj, function () {
-        on_off_button.innerHTML = 'Start'
-        on_off_button.className = 'green'
+      chrome.storage.sync.set(newObj, function() {
+        on_off_button.innerHTML = "Start";
+        on_off_button.className = "green";
       });
-
-    } else { //turn extention on for current tab
+    } else {
+      //turn extention on for current tab
       activeTabs.push(currentTab);
       let newObj = {
-        activeTabs: activeTabs,
-      }
+        activeTabs: activeTabs
+      };
 
-      chrome.storage.sync.set(newObj, function () {
-        on_off_button.innerHTML = 'Stop'
-        on_off_button.className = 'red'
+      chrome.storage.sync.set(newObj, function() {
+        on_off_button.innerHTML = "Stop";
+        on_off_button.className = "red";
       });
     } //else
-  }) //get.sync.storage.chrome
-} //onclick
+  }); //get.sync.storage.chrome
+}; //onclick
 
-pause_button.onclick = function () {
-  chrome.storage.sync.get(['paused'], function (result) { //fetch active tabs from storage
-    paused = !result['paused'];
+pause_button.onclick = function() {
+  chrome.storage.sync.get(["paused"], function(result) {
+    //fetch active tabs from storage
+    paused = !result["paused"];
 
     let newText;
     if (paused) {
@@ -107,12 +105,22 @@ pause_button.onclick = function () {
     }
 
     let newState = {
-      paused: paused,
-    }
+      paused: paused
+    };
 
-    chrome.storage.sync.set(newState, function () {
+    chrome.storage.sync.set(newState, function() {
       pause_button.innerHTML = newText;
     });
     return;
   });
-}
+};
+
+// Show / Hide local files link if needed
+
+chrome.extension.isAllowedFileSchemeAccess(localAllowed => {
+  if(!localAllowed){
+    const localLink = document.getElementById("localLink");
+    localLink.style.display = "block"
+  }
+  
+});
